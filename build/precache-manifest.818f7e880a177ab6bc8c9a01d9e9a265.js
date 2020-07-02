c19f1472f1a71b07ac9a37916688f0acfda9fdac
kpinpa173 <durga_pinpala@comcast.com>
2020-07-02 13:58:06 -0400

    update package.json & build update

---
+++ build/precache-manifest.818f7e880a177ab6bc8c9a01d9e9a265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd78e712807dbbe4846c576dfa2375ba",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "6ee7a4a57a2d3f21e371",
+    "url": "/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdcab2610e626414cde3",
+    "url": "/my-first-react-app/static/js/2.067f0017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.067f0017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee7a4a57a2d3f21e371",
+    "url": "/my-first-react-app/static/js/main.17416c9f.chunk.js"
+  },
+  {
+    "revision": "1b6bd1ce0b42710c3677",
+    "url": "/my-first-react-app/static/js/runtime-main.3cdad74c.js"
+  }
+]);
