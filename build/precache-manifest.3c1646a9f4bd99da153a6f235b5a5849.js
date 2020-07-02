a8025a0f4be6904952f3588e203faa2f3a7f5bbd
kpinpa173 <durga_pinpala@comcast.com>
2020-07-02 13:46:17 -0400

    adding build

---
+++ build/precache-manifest.3c1646a9f4bd99da153a6f235b5a5849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40af8894204da467a141946585af60d0",
+    "url": "/pkanakad27/my-first-react-app/index.html"
+  },
+  {
+    "revision": "0fe7c6df93ba41ab132e",
+    "url": "/pkanakad27/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdcab2610e626414cde3",
+    "url": "/pkanakad27/my-first-react-app/static/js/2.067f0017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pkanakad27/my-first-react-app/static/js/2.067f0017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe7c6df93ba41ab132e",
+    "url": "/pkanakad27/my-first-react-app/static/js/main.35201956.chunk.js"
+  },
+  {
+    "revision": "ca205c6ea93aa788c166",
+    "url": "/pkanakad27/my-first-react-app/static/js/runtime-main.12a1cf87.js"
+  }
+]);
